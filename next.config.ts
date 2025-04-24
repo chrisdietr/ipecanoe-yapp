4b976283013048487b9b5e8a58a2503f7d199a6a
Andreas Oee <andy.oeee@gmail.com>
2025-04-24 18:37:21 +0700

    feat: add Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  headers: async () => {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
