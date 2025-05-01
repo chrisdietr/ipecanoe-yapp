5f669378b694a2db3c3f47e360efffe8457602d3
Your Name <you@example.com>
2025-05-01 18:04:07 -0300

    low price for testing

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,8 +2,8 @@ export const YAPP_ENS = "canoe-shop.ipecity.eth";
 export const RECEIVER_ADDRESS = "0xB2c31e855C8D092E2A2f877DFB154Abb4793F9F6";
 export const RECEIVER_ENS = "koruvaa.ipecity.eth";
 export const DEFAULT_PAYMENT_CURRENCY = "BRL";
-export const DEFAULT_PAYMENT_AMOUNT = 60; // TODO: set to 60 in prod
-export const APP_URL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://ipecanoe-yapp-tau.vercel.app";
+export const DEFAULT_PAYMENT_AMOUNT = 0.1; // TODO: set to 60 in prod
+export const APP_URL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://ipecanoe-yapp-tau.vercel.app/";
 
 export const EARLY_TIME = "06:00";
 export const LATE_TIME = "07:30";
