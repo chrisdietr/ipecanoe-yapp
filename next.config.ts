ccb08202a308980496094869829937ad27cbc6d2
chrisdietr <cdimconsulting@gmail.com>
2025-05-12 13:28:39 +0000

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    serverActions: true,
-  }
+  experimental: {},
 };
 
 export default nextConfig;
